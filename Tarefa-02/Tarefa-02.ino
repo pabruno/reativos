280438f789f4c0e667574c98b35405fc06f74811
pabruno <nozesffc@gmail.com>
2016-03-20 23:19:50 -0300

    Added files via upload

---
+++ Tarefa-02/Tarefa-02.ino
@@ -0,0 +1,54 @@
+#define RedPin 2
+#define ButPinAcelera 13
+#define ButPinDiminui 12
+int state = 1;
+int tempo = 1000;
+unsigned long old;
+int butAcelera;
+int UltimoClickAcelera = 0;
+int butDiminui;
+int UltimoClickDiminui = 0;
+unsigned long now;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RedPin,OUTPUT);
+  pinMode(ButPinAcelera,INPUT);
+  pinMode(ButPinDiminui,INPUT);
+  digitalWrite(RedPin,state);
+  old = millis();
+}
+
+void loop() {
+  unsigned long now = millis();
+  Serial.println(tempo);
+  if(tempo < 0) {
+    tempo = 0;
+  }
+  if(now >= old+tempo) {
+    old = now;
+    state = !state;
+    digitalWrite(RedPin,state);
+  }
+
+  butAcelera = digitalRead(ButPinAcelera);
+  butDiminui = digitalRead(ButPinDiminui);
+  
+  if(butAcelera && (now - UltimoClickAcelera) > 150) {
+    tempo -= 10;
+    UltimoClickAcelera = now;
+  }
+
+  if(butDiminui && (now - UltimoClickDiminui) > 150){
+    tempo += 10;
+    UltimoClickDiminui = now;
+  }
+
+   if(butAcelera && butDiminui) {
+    if(UltimoClickDiminui - UltimoClickAcelera < 500) {
+      Serial.print("Fim");
+      digitalWrite(RedPin,HIGH);
+      while(1);
+    }
+  }
+}
