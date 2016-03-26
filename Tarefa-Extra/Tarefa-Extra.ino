f1a8f1f550bad07cf386d3c7cbb3969ddb22af71
pabruno <nozesffc@gmail.com>
2016-03-26 19:40:17 -0300

    Added files via upload

---
+++ Tarefa-Extra/Tarefa-Extra.ino
@@ -0,0 +1,43 @@
+#define TempPin A5
+#define OrangePin 2
+#define YellowPin 4
+#define RedPin 7
+float temp;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TempPin,INPUT);
+  pinMode(OrangePin,OUTPUT);
+  pinMode(YellowPin,OUTPUT);
+  pinMode(RedPin,OUTPUT);
+}
+
+void loop() {
+  temp = (analogRead(TempPin)*5)/10.23;
+  Serial.print("Temperatura: ");
+  Serial.println(temp);
+
+  if(temp > 0.0 && temp < 33) {
+    digitalWrite(OrangePin,HIGH);
+  }
+  else if (temp < 0.0) {
+    digitalWrite(OrangePin,LOW);
+  }
+
+  if(temp > 33 && temp < 36) {
+    digitalWrite(OrangePin,HIGH);
+    digitalWrite(YellowPin,HIGH);
+  }
+  else if(temp < 36) {
+    digitalWrite(YellowPin,LOW);
+  }
+
+  if(temp > 36) {
+    digitalWrite(OrangePin,HIGH);
+    digitalWrite(YellowPin,HIGH);
+    digitalWrite(RedPin,HIGH);
+  }
+  else {
+    digitalWrite(RedPin,LOW);
+  }
+}
